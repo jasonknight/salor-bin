0078c29be0909701badb0331c025710f930dff93
Jason Martin <jason@jason-knight-martin.com>
2011-07-04 17:00:40 +0200

    xxx

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Thu Jun 30 20:19:42 2011
+** Created: Sat Jul 2 02:16:35 2011
 **      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
