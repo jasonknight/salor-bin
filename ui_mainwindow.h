5c5f23497ee9f2fd48ff2b21d4fc64c8d9aa7139
jasonknight <jasonknight@jasonknight-VirtualBox.(none)>
2011-08-08 15:13:24 +0200

    salor paage

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Thu Jul 7 18:24:55 2011
-**      by: Qt User Interface Compiler version 4.6.2
+** Created: Thu Aug 4 18:55:01 2011
+**      by: Qt User Interface Compiler version 4.7.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
