97c2e1a34079bf6e84947da0e20ddc3925070391
Jason Knight <jason@jason-knight-martin.com>
2011-10-31 00:56:07 +0100

    workingpaylife

--- paylife.cpp
+++ paylife.cpp
@@ -9,28 +9,34 @@ PayLife::PayLife(QObject *parent) :
 }
 void PayLife::run() {
   qDebug() << "In Thread, returning: Data: " << this->data << " and addy: " << this->addy;
+  this->sendingData = 0;
     int fd;
     int count;
     int i;
 	char buff[132];
-     this->descriptor = open_serial_port(this->addy.toLatin1().data());
+     this->descriptor = open_paylife(this->addy.toLatin1().data());
 	if (this->descriptor <= 0) {
-		qDebug() << "Could not open device!";
-            } else {
-                qDebug() << "Opened device: " << QString::number(this->descriptor) << "\n";
-            }
+            qDebug() << "Could not open device!";
+        }
   while (this->descriptor > 0) {
+      if (this->sendingData == 1) {
+        sleep(1);
+        continue;
+      }
 	  count = read(this->descriptor,&buff,132);
-    if (count > 1) {
-      qDebug() << "Received: " << QString(buff);
-    }
+          if (buff[0] != 0x05) {
+              for (i = 0; i < strlen(buff); i++) {
+                printf(" %x ", buff[i]);
+              }
+              printf("EOT\n\n");
+          }
     write(this->descriptor, "\x06",1);
-    usleep(100000);
+    sleep(4);
   }
      close(fd);
 }
 void PayLife::sendPayLifeData(QString data) {
-
+    this->sendingData = 1;
   int len = data.length();
   int i = 0;
   char lrc = 0;
@@ -40,31 +46,38 @@ void PayLife::sendPayLifeData(QString data) {
   char * bytes;
   int ret;
   char buffer[132];
+  char inbuffer[132];
 
-  bytes = (char *) malloc(sizeof(char) * len + 1);
+  bytes = (char *) malloc(sizeof(char) * len + 8);
   strcpy(bytes,data.toLatin1().data());
   qDebug() << "PayLife Sending data: " << data << " of len " << QString::number(len) <<
           " " << " Bytes is: " << bytes ;
-  while (i < len - 1) {
+  sprintf(bytes,"%s%c",bytes,etx);
+  while (i < len + 1) {
     lrc ^= bytes[i];
     i++;
   }
   printf("LRC: %c\n",lrc);
-  /* sprintf(buffer,"%c%c%s%c%c",ack,stx,bytes,etx,lrc);
+  sprintf(buffer,"%c%s%c%c",stx,bytes,lrc);
   for (i = 0; i < strlen(buffer); i++) {
-    printf("c: %X ", buffer[i]);
+    printf(" [%c] ",buffer[i]);
   }
-  printf(" [[ %s ]] \n", buffer); */
-  qDebug() << "Starting";
-  write(this->descriptor,&ack,1);
-  usleep(100000);
-  write(this->descriptor,&stx,1);
-  usleep(100000);
-  ret = write(this->descriptor,bytes,len - 1);
-  usleep(100000);
-  write(this->descriptor,&etx,1);
-  usleep(100000);
-  write(this->descriptor,&lrc,1);
+  qDebug() << "\nStarting";
+  read(this->descriptor,&inbuffer,1);
+  while (inbuffer[0] != 0x05) {
+    read(this->descriptor,&inbuffer,132);
+  }
+  printf("ENQ received\n");
+  write(this->descriptor,buffer,len + 4);
+  //usleep(100000);
+  sleep(1);
+  printf("Received: ");
+  read(this->descriptor,&inbuffer,132);
+  for (i = 0; i < strlen(inbuffer); i++) {
+    printf(" %x ",inbuffer[i]);
+  }
+  printf("\n");
   qDebug() << "Done";
   free(bytes);
+  this->sendingData = 0;
 }
--- paylife.h
+++ paylife.h
@@ -2,7 +2,12 @@
 #define PAYLIFE_H
 
 #include <QThread>
-
+#include <stdio.h>   /* Standard input/output definitions */
+#include <string.h>  /* String function definitions */
+#include <unistd.h>  /* UNIX standard function definitions */
+#include <fcntl.h>   /* File control definitions */
+#include <errno.h>   /* Error number definitions */
+#include <termios.h> /* POSIX terminal control definitions */
 class PayLife : public QThread
 {
     Q_OBJECT
@@ -11,6 +16,7 @@ public:
     QString addy;
     QString data;
     QString error;
+    int sendingData;
     int descriptor;
     int errorNumber;
     void run();
@@ -21,5 +27,31 @@ signals:
 public slots:
   void sendPayLifeData(QString data);
 };
+static int open_paylife(char *port) {
 
+  int fd;
+  struct termios options;
+
+  fd = open(port, O_RDWR | O_NOCTTY | O_NDELAY);
+
+  if (fd == -1) {
+
+    printf("Unable to open port %s", port);
+
+  } else {
+
+    tcgetattr(fd, &options); // Get the current options for the port...
+    cfsetispeed(&options, B1200); // Set the baud rates
+    cfsetospeed(&options, B1200);
+    //printf("c_cflag = %X \n", options.c_cflag);
+    options.c_cflag &= ~CSIZE;
+    //printf("c_cflag = %X \n", options.c_cflag);
+    options.c_cflag |= (CLOCAL | CREAD | CS7 | PARENB); // Enable the receiver and set local mode...  | CS7 | PARENB
+    //printf("c_cflag = %X \n", options.c_cflag);
+    tcsetattr(fd, TCSANOW, &options); // Set the new options for the port...
+    //fcntl(fd, F_SETFL, 0); // seems to enable blocking mode
+  }
+
+  return(fd);
+}
 #endif // PAYLIFE_H
