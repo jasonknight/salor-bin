ac12cbf44878d92ec801296f325265e45fa906ec
Jason Knight <jason@jason-knight-martin.com>
2012-01-20 11:37:16 +0100

    various

--- cashdrawer.cpp
+++ cashdrawer.cpp
@@ -14,7 +14,7 @@ void CashDrawer::run() {
     char cash_drawer_closed[5] = "\x14\x00\x00\x0f";
     int cap = 500;
     int x = 0;
-    qDebug() << "XXX: Writing open cash drawer too: " << addy;
+    //qDebug() << "XXX: Writing open cash drawer too: " << addy;
     //printf("XXX Writing open drawer %s \n",addy.toLatin1().data());
     fd = open_serial_port(addy.toLatin1().data());
     if (fd <= 0) {
@@ -39,8 +39,8 @@ void CashDrawer::run() {
         }
       usleep(20000); //i.e. 20m
       count = read(fd,&buf,19);
-      printf("-- %i \n", count);
-      for (i=0;i < count; i++) { printf("%X ", *(buf+i));}
+      //printf("-- %i \n", count);
+      //for (i=0;i < count; i++) { printf("%X ", *(buf+i));}
       if (x == cap) {
         printf("XXX cap reached \n");
         close_fd(fd);
