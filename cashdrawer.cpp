774d2cd060270fe38f7362f7f8df6501285bb9f4
Jason Knight <jason@jason-knight-martin.com>
2012-01-25 19:12:12 +0100

    various

--- cashdrawer.cpp
+++ cashdrawer.cpp
@@ -1,6 +1,14 @@
 #include "cashdrawer.h"
 #include "scales.h"
 #include <QDebug>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <linux/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/time.h>
 CashDrawer::CashDrawer(QObject *parent) :
     QThread(parent)
 {
@@ -19,26 +27,35 @@ void CashDrawer::run() {
     fd = open_serial_port(addy.toLatin1().data());
     if (fd <= 0) {
         qDebug() << "CashDrawer failed to open!";
+        return;
     }
+
     write(fd, "\x1D\x61\xFF", 3);
     usleep(2000); //i.e. 20ms
     write(fd, "\x1B\x70\x00\xFF\x01", 5);
-    read(fd, &buf, 19);
-    usleep(2000); //i.e. 20ms
-    read(fd, &buf, 19);
-    strcpy(buf,"");
-    usleep(2000);
-    read(fd, &buf, 19);
-    strcpy(buf,"");
-    read(fd, &buf, 19);
+    fd_set f;
+    FD_ZERO(&f);
+    FD_SET(fd,&f);
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 100;
     for (i=0;i < count; i++) { printf("%X ", *(buf+i));}
     while (strcmp(cash_drawer_closed,buf) != 0) {
         if (this->running == false) {
             close_fd(fd);
             return;
         }
-      usleep(20000); //i.e. 20m
-      count = read(fd,&buf,19);
+      int i = select(fd + 1, &f,NULL,NULL,&tv);
+      if (i>0) {
+          if (FD_ISSET(fd,&f)) {
+              count = read(fd,&buf,19);
+          } else {
+              count = 0;
+          }
+      } else {
+        count = 0;
+      }
+
       //printf("-- %i \n", count);
       //for (i=0;i < count; i++) { printf("%X ", *(buf+i));}
       if (x == cap) {
