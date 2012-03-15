ea2ca5e56ec54fd80caf4eae5c421f41c3821cd0
root <root@zotac.(none)>
2012-03-15 15:37:37 +0100

    added cash drawer latch

--- cashdrawer.cpp
+++ cashdrawer.cpp
@@ -16,11 +16,14 @@ DrawerObserverThread::DrawerObserverThread(QObject *parent) : QThread(parent)
 void DrawerObserverThread::run() {
     this->running = true;
     int fd;
-    int count;
+    int count = 0;
     int i = 0;
     int close_after_seconds = 30;
+
     char buf[20];
-    //char cash_drawer_closed[5] = "\x14\x00\x00\x0f";
+    char closed_code[5] = "\x14\x00\x00\x0f";
+    char opened_code[5] = "\x10\x00\x00\x0f";
+    bool drawer_was_open = false;
 
     stop_drawer_thread = false;
     fd = open_serial_port_for_drawer(addy.toLatin1().data());
@@ -30,19 +33,34 @@ void DrawerObserverThread::run() {
     } else {
       qDebug() << QString("Opened file descriptor 0x%1 for cash drawer.").arg(uchar(fd),0,16);
     }
-    count = read(fd, buf, 20); // clear serial buffer
-    qDebug() << "Cleared" << count << "bytes from the serial buffer on the first run.";
-    usleep(200000); // the printer will wait for a clear buffer to send any pending bytes...
-    count = read(fd, buf, 20); // ... so clear serial buffer again
-    qDebug() << "Cleared" << count << "bytes from the serial buffer on the second run.";
-    count = 0;
-    while (i < (5 * close_after_seconds) && count <= 0 && !stop_drawer_thread) {
+    //count = read(fd, buf, 20); // clear serial buffer
+    //qDebug() << "Cleared" << count << "bytes from the serial buffer on the first run.";
+    //usleep(200000); // the printer will wait for a clear buffer to send any pending bytes...
+    //count = read(fd, buf, 20); // ... so clear serial buffer again
+    //qDebug() << "Cleared" << count << "bytes from the serial buffer on the second run.";
+
+    while (i < (2 * close_after_seconds) && !stop_drawer_thread) {
       i += 1;
       count = read(fd, buf, 7);
-      qDebug() << (5*close_after_seconds-i) <<"Read numer of bytes from drawer: " << count;
+
+      if (!drawer_was_open && strcmp(opened_code, buf) == 0 ) {
+          drawer_was_open = true;
+          qDebug() << "Open Drawer detected.";
+      }
+      if (drawer_was_open && strcmp(closed_code, buf) == 0) {
+          stop_drawer_thread = true;
+          qDebug() << "Closed Drawer detected. Halting thread.";
+      }
+      if (drawer_was_open) {
+          qDebug() << (5*close_after_seconds-i) << "Waiting for Drawer close. Bytes from drawer: " << count;
+      } else {
+          qDebug() << (5*close_after_seconds-i) << "Waiting for Drawer open. Bytes from drawer: " << count;
+      }
+
+      //count = 0;
       //int j = 0;
       //for (j=0;j<count;j++) { printf("%X|",*(buf+j)); } // debug
-      usleep(200000);
+      usleep(500000);
     }
     close(fd);
     cashDrawerClosed();
