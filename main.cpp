188eae58a2161300455c639f915ca630d5c65a41
Michael Franzl <michaelfranzl@gmx.at>
2012-08-22 16:14:17 +0200

    new salor link

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.to_url = QString("http://salor/orders/new");
+    w.to_url = QString("http://salor-retail");
     QString arg;
     bool fs = true;
     for (int i = 1; i < argc; i++) {
