6772f494258370814433abd744c2eff92c4ad1bc
Michael Franzl <office@michaelfranzl.com>
2013-03-09 12:39:19 +0100

    re-added debian dir

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(salor-bin):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="salor-bin" command="/usr/bin/salor-bin"
---
+++ debian/salor-bin.default.ex
@@ -0,0 +1,10 @@
+# Defaults for salor-bin initscript
+# sourced by /etc/init.d/salor-bin
+# installed at /etc/default/salor-bin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
