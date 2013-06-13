03337f96edf00f8b7f79f4af3c016dbfd2c706ba
Michael Franzl <office@michaelfranzl.com>
2013-06-13 13:42:49 +0200

    added cronjob for log file purge

--- debian/salor-bin.cron.d
+++ debian/salor-bin.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the salor-bin package
 #
-0 4	* * *	root	[ -x /usr/bin/salor-bin_maintenance ] && /usr/bin/salor-bin-maintenance
+0 4	* * *	root	[ -x /usr/bin/salor-bin-maintenance ] && /usr/bin/salor-bin-maintenance
